5474005bfb87a7cf8db13c6336353a69aceabe37
Jon Chin <jon@sharemeals.org>
2022-07-13 10:18:12 -0400

    fixed typo

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -58,79 +58,79 @@ module.exports = () => {
                     fontColor: {
                         columns: 5,
                         documentColors: 10,
-                      },
-                      fontBackgroundColor: {
+                    },
+                    fontBackgroundColor: {
                         columns: 5,
                         documentColors: 10,
-                      },
+                    },
                     image: {
                         resizeUnit: "%",
                         resizeOptions: [ {
-                          name: 'resizeImage:original',
-                          value: null,
-                          icon: 'original'
+                            name: 'resizeImage:original',
+                            value: null,
+                            icon: 'original'
                         },
-                        {
-                          name: 'resizeImage:25',
-                          value: '25',
-                          icon: 'small'
-                        },
-                        {
-                          name: 'resizeImage:50',
-                          value: '50',
-                          icon: 'medium'
-                        },
-                        {
-                          name: 'resizeImage:75',
-                          value: '75',
-                          icon: 'large'
-                        } ],
+					 {
+					     name: 'resizeImage:25',
+					     value: '25',
+					     icon: 'small'
+					 },
+					 {
+					     name: 'resizeImage:50',
+					     value: '50',
+					     icon: 'medium'
+					 },
+					 {
+					     name: 'resizeImage:75',
+					     value: '75',
+					     icon: 'large'
+					 } ],
                         toolbar: [
-                          'toggleImageCaption',
-                          'imageTextAlternative',
-                          'imageStyle:inline',
-                          'imageStyle:block',
-                          'imageStyle:side',
-                          'linkImage',
-                          'resizeImage:25', 'resizeImage:50', 'resizeImage:75', 'resizeImage:original'
+                            'toggleImageCaption',
+                            'imageTextAlternative',
+                            'imageStyle:inline',
+                            'imageStyle:block',
+                            'imageStyle:side',
+                            'linkImage',
+                            'resizeImage:25', 'resizeImage:50', 'resizeImage:75', 'resizeImage:original'
                         ]
-                      },
-                      // https://ckeditor.com/docs/ckeditor5/latest/features/table.html
-                      table: {
+                    },
+                    // https://ckeditor.com/docs/ckeditor5/latest/features/table.html
+                    table: {
                         contentToolbar: [
-                          'tableColumn',
-                          'tableRow',
-                          'mergeTableCells',
-                          'tableCellProperties',
-                          'tableProperties',
-                          'toggleTableCaption'
+                            'tableColumn',
+                            'tableRow',
+                            'mergeTableCells',
+                            'tableCellProperties',
+                            'tableProperties',
+                            'toggleTableCaption'
                         ]
-                      },
-                      // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html
-                      heading: {
+                    },
+                    // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html
+                    heading: {
                         options: [
-                          { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                          { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                          { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-                          { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-                          { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
                         ]
-                      },
-                      // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html
-                      htmlSupport: {
+                    },
+                    // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html
+                    htmlSupport: {
                         allow: [
                             {
-                              name: 'img',
-                              attributes: {
-                                  sizes:true,
-                                  loading:true,
-                              }
+				name: 'img',
+				attributes: {
+                                    sizes:true,
+                                    loading:true,
+				}
                             },
                         ]
-                      },
+                    },
                 }
             }
-        }
+        },
 	seo: true
     }
 }
