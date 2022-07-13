eb2a6790840cc394d8bc0d475166669ae64206ee
AdrianPerez28 <adrianp2889@gmail.com>
2022-07-13 10:17:46 -0400

    updated article to toolkit page

---
+++ strapi/src/api/toolkit-page/services/toolkit-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * toolkit-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::toolkit-page.toolkit-page');
