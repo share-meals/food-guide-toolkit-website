26ca854757384c7455f671db9257307ce21e450f
Adrian <76135853+Quipiii@users.noreply.github.com>
2022-07-13 10:31:40 -0400

    created article branch for new collection type (#18)
    * created article branch for new collection type
    
    * updated article to toolkit page

---
+++ strapi/src/api/toolkit-page/routes/toolkit-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * toolkit-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::toolkit-page.toolkit-page');
