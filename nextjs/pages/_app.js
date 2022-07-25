048c5c23c80ca320c54eab0dfd1d54e6628a6861
Jon Chin <jon@sharemeals.org>
2022-07-25 10:38:19 -0400

    added example of how to setup custom themes in MUI

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,14 +1,29 @@
 import '../styles/globals.css'
 import Header from '../components/Header.js';
+import {
+    createTheme,
+    ThemeProvider
+} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+	primary: {main: '#DDAACC'},
+	secondary: {main: '#6AA84F'},
+	warning: {main: '#0CF3B9'},
+	error: {main: '#F3560C'}
+    }
+});
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <div className='MyApp'>
-      <Header />
-      
-      <Component {...pageProps} />
-    </div>
-  );
+    return(
+	<ThemeProvider theme={theme}>
+	    <div className='MyApp'>
+		<Header />
+		
+		<Component {...pageProps} />
+	    </div>
+	</ThemeProvider>
+    );
 }
 
 export default MyApp
